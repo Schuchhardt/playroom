7bf74ebf251a6513702514781a0d79de3bf79fdb
Sebastian Schuchhardt <schgressive@gmail.com>
2020-08-05 19:26:28 -0400

    fix esmodule

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,4 +5,6 @@ const vue = require('./loaders/vue')
 environment.loaders.append('vue', vue)
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
+environment.loaders.get('file').use.find(item => item.loader === 'file-loader').options.esModule = false
+
 module.exports = environment
